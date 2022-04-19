0350b23b6b39be33a5f9ddd6bcbdfb1d85484a5b
alexeng <ae5590@princeton.edu>
2022-04-18 21:18:22 -0400

    randomcharv1

---
+++ randomchar.c
@@ -0,0 +1,23 @@
+/*--------------------------------------------------------------------*/
+/* randomchar.c                                                       */
+/* Author: Calvin Nguyen and Alex Eng                                 */
+/*--------------------------------------------------------------------*/
+
+#include <assert.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+
+int main(int argc, char **argv)
+{   
+    for (int i = 0; i < 1000; i++)
+    {
+        int x = rand();
+        /*if ((x >= 0) && (x < ))*/
+            printf("%c\n", "");
+        /*else if*/
+    }
+    
+    
+    return 0;
+}
